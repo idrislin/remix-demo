d4c7d810c7d29d744802fe5a0460a2b61202971c
linjx <2376909468@qq.com>
2024-04-25 18:59:23 +0800

    🚧 first commit

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <div id="sidebar">{/* other elements */}</div>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
